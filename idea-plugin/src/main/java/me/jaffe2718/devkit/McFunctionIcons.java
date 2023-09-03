0b35eaaaeffd517c8e851d9d1d32c7a44f104d22
Jaffe2718 <qqyttwqeei@163.com>
2023-09-03 21:35:25 +0800

    1.0.3: vectorized icon

--- idea-plugin/src/main/java/me/jaffe2718/devkit/McFunctionIcons.java
+++ idea-plugin/src/main/java/me/jaffe2718/devkit/McFunctionIcons.java
@@ -5,6 +5,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public abstract class McFunctionIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/mcfunction.png", McFunctionIcons.class);
+    public static final Icon FILE = IconLoader.getIcon("/icons/mcfunction.svg", McFunctionIcons.class);
     public static final String TOOL_NAME = "ide-debug-tool-1.1.2.jar";
 }
