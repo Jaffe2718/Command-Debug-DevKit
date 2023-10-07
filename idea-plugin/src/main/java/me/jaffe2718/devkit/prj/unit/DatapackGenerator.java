45aeace83fb535ac477f3bd62ec5e019fa453059
Jaffe2718 <qqyttwqeei@163.com>
2023-10-07 09:10:40 +0800

    plugin 1.2.1-rc1: fix bug --> the datapack zip file created by the plugin cannot be loaded by the game

--- idea-plugin/src/main/java/me/jaffe2718/devkit/prj/unit/DatapackGenerator.java
+++ idea-plugin/src/main/java/me/jaffe2718/devkit/prj/unit/DatapackGenerator.java
@@ -1,15 +1,12 @@
 package me.jaffe2718.devkit.prj.unit;
 
 import com.intellij.openapi.project.Project;
+import net.lingala.zip4j.ZipFile;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Objects;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public class DatapackGenerator {
     private final Project project;
@@ -26,53 +23,23 @@ public class DatapackGenerator {
         Path buildDirPath = prjRoot.resolve("build");                  // find `build` dir
         String pkName = this.project.getName() + ".zip";                     // the name of the zip file
 
-        // check if the pack.mcmeta file and data dir exist
         if (!packMetaPath.exists() || !dataDirPath.exists()) {
             return CheckState.INVALID;
-        } else {
-            buildDirPath.toFile().mkdirs();
-            buildDirPath.resolve(pkName).toFile().delete();
-            try (FileOutputStream fos = new FileOutputStream(buildDirPath.resolve(pkName).toFile());
-                 ZipOutputStream zos = new ZipOutputStream(fos)) {
-                // add pack.mcmeta
-                this.addFileToZip(packMetaPath, "pack.mcmeta", zos);
-                this.addDirToZip(dataDirPath, "data", zos);
-                if (!packPngPath.exists()) {
-                    zos.flush();
-                    zos.finish();
-                    zos.close();
-                    return CheckState.VALID_WITH_WARNINGS;
-                }
-                else {
-                    this.addFileToZip(packPngPath, "pack.png", zos);
-                    zos.flush();
-                    zos.finish();
-                    zos.close();
-                    return CheckState.VALID;
-                }
-            } catch (IOException e) {
-                return CheckState.INVALID;
-            }
         }
+        try {
+            ZipFile datapack = new ZipFile(buildDirPath.resolve(pkName).toFile());
+            datapack.addFile(packMetaPath);
+            datapack.addFolder(dataDirPath);
+            if (packPngPath.exists()) {
+                datapack.addFile(packPngPath);
+            } else {
+                datapack.close();
+                return CheckState.VALID_WITH_WARNINGS;
+            }
+        } catch (IOException e) {
+            return CheckState.INVALID;
+        }
+        return CheckState.VALID;
     }
 
-    private void addFileToZip(File srcFile, String innerPth, ZipOutputStream zos) throws IOException {
-        zos.putNextEntry(new ZipEntry(innerPth));
-        FileInputStream fis = new FileInputStream(srcFile);
-        byte[] buffer = new byte[1024];
-        int len;
-        while ((len = fis.read(buffer)) > 0) {
-            zos.write(buffer, 0, len);
-        }
-    }
-
-    private void addDirToZip(File srcPth, String innerPth, ZipOutputStream zos) throws IOException {
-        if (srcPth.isDirectory()) {
-            for (File file : Objects.requireNonNull(srcPth.listFiles())) {
-                addDirToZip(file, Path.of(innerPth, file.getName()).toString(), zos);
-            }
-        } else {
-            addFileToZip(srcPth, innerPth, zos);
-        }
-    }
 }
